668362e17c08033fc82562b9d6ded76c0df823aa
STNeto1 <gfreitasneto18@gmail.com>
2023-04-19 19:10:59 -0300

    connect and close connection on channel/topic

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,27 @@
+use axum::{
+    extract::{
+        ws::{Message, WebSocket},
+        Path, State, WebSocketUpgrade,
+    },
+    response::IntoResponse,
+    routing, Router,
+};
+use futures::{SinkExt, StreamExt};
+use serde::{Deserialize, Serialize};
 use std::{net::SocketAddr, sync::Arc};
-
-use axum::Router;
+use tokio::sync::broadcast;
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
 
-struct AppState {}
+#[derive(Debug, Serialize, Deserialize, Clone)]
+struct WsMessage {
+    channel: String,
+    topic: String,
+    value: serde_json::Value,
+}
+
+struct AppState {
+    tx: broadcast::Sender<WsMessage>,
+}
 
 #[tokio::main]
 async fn main() {
@@ -15,9 +33,12 @@ async fn main() {
         .with(tracing_subscriber::fmt::layer())
         .init();
 
-    let app_state = Arc::new(AppState {});
+    let (tx, _rx) = broadcast::channel::<WsMessage>(100);
 
-    let app = Router::new().with_state(app_state);
+    let app_state = Arc::new(AppState { tx });
+    let app = Router::new()
+        .route("/ws/:channel/:topic", routing::get(websocket_handler))
+        .with_state(app_state);
 
     let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
     tracing::debug!("listening on {}", addr);
@@ -25,4 +46,62 @@ async fn main() {
         .serve(app.into_make_service())
         .await
         .unwrap();
+}
+
+async fn websocket_handler(
+    ws: WebSocketUpgrade,
+    Path((channel, topic)): Path<(String, String)>,
+    State(state): State<Arc<AppState>>,
+) -> impl IntoResponse {
+    ws.on_upgrade(|socket| websocket(channel, topic, socket, state))
+}
+
+async fn websocket(channel: String, topic: String, stream: WebSocket, state: Arc<AppState>) {
+    // By splitting, we can send and receive at the same time.
+    let (mut sender, mut receiver) = stream.split();
+
+    if let Err(_) = sender.send(Message::Ping("Ping".into())).await {
+        tracing::debug!("could not ping");
+        return;
+    }
+
+    // We subscribe *before* sending the "joined" message, so that we will also
+    // display it to our client.
+    let mut rx = state.tx.subscribe();
+
+    // Spawn the first task that will receive broadcast messages and send text
+    let mut send_task = tokio::spawn(async move {
+        while let Ok(msg) = rx.recv().await {
+            if msg.channel == channel && msg.topic == topic {
+                if sender
+                    .send(Message::Text(serde_json::to_string(&msg.value).unwrap()))
+                    .await
+                    .is_err()
+                {
+                    break;
+                }
+            }
+        }
+    });
+
+    let mut recv_task = tokio::spawn(async move {
+        while let Some(Ok(msg)) = receiver.next().await {
+            // End connection if client sends a close frame
+            if msg == Message::Close(None) {
+                break;
+            }
+        }
+    });
+
+    //wait for either task to finish and kill the other task
+    tokio::select! {
+        _ = (&mut send_task) => {
+            recv_task.abort();
+        },
+        _ = (&mut recv_task) => {
+            send_task.abort();
+        }
+    }
+
+    tracing::debug!("websocket connection closed");
 }
